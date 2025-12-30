eaf8ff3dcba9112b4f8fe6adc8b2ecb96ee60619
Themis Baltzakis <tbaltzakis@cloudless.gr>
2025-12-30 13:11:51 +0200

    Fix ESLint errors in create-slack-channel.js
    - Replace require() statements with ES module import syntax
    - Use proper import statements for dotenv and @slack/web-api
    - Remove transpiled import syntax

--- create-slack-channel.js
+++ create-slack-channel.js
@@ -1,13 +1,13 @@
 "use strict";
-var import_dotenv = require("dotenv");
-var import_web_api = require("@slack/web-api");
-(0, import_dotenv.config)({ path: ".env.local" });
+import dotenv from "dotenv";
+import { WebClient } from "@slack/web-api";
+dotenv.config({ path: ".env.local" });
 const token = process.env.SLACK_BOT_TOKEN;
 if (!token) {
   console.error("SLACK_BOT_TOKEN environment variable is not set");
   process.exit(1);
 }
-const client = new import_web_api.WebClient(token);
+const client = new WebClient(token);
 async function createChannel() {
   try {
     const result = await client.conversations.create({
