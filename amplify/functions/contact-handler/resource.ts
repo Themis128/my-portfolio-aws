fce221306b94b8f3bff93438cf171207f6e42aac
Themis Baltzakis <tbaltzakis@cloudless.gr>
2026-01-02 22:59:37 +0200

    Fix contact form GraphQL endpoint URLs
    - Update contact-handler function environment variables with correct GraphQL endpoint and API key
    - Fix hardcoded URLs that were causing ENOTFOUND errors
    - Contact form should now work properly with email sending and Slack notifications

--- amplify/functions/contact-handler/resource.ts
+++ amplify/functions/contact-handler/resource.ts
@@ -6,7 +6,7 @@ export const contactHandler = defineFunction({
   runtime: 20,
   environment: {
     SLACK_WEBHOOK_URL: 'REDACTED_SLACK_WEBHOOK_URL',
-    AMPLIFY_DATA_GRAPHQL_ENDPOINT: 'https://ggbslhgtjbgkzcnbm7kfq3z6ku.appsync-api.eu-central-1.amazonaws.com/graphql',
-    AMPLIFY_DATA_API_KEY: 'da2-4sp2psirnncn7lgrly3bndxksy',
+    AMPLIFY_DATA_GRAPHQL_ENDPOINT: 'https://kl4own6nqnegdfliofccu5klza.appsync-api.eu-central-1.amazonaws.com/graphql',
+    AMPLIFY_DATA_API_KEY: 'da2-gkzrnvmpgrclhfmtocc4melqye',
   },
 });
